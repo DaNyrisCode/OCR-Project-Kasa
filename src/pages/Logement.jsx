8ac92ef3cbbd3050068977af463327af2bec6980
DaNyrisCode <komakhinamarina@gmail.com>
2024-11-28 10:08:16 +0100

    Verif ID logements pour la p 404

--- src/pages/Logement.jsx
+++ src/pages/Logement.jsx
@@ -1,5 +1,5 @@
 import data from '../data/rental.json';
-import { useParams } from "react-router-dom";
+import { useParams, Navigate } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import Diapo from '../components/Slideshow';
 import MainComponentRental from '../components/MainComponentRental';
@@ -7,32 +7,41 @@ import InfoRental from '../components/InfoRental';
 
 const Logement = () => {
     const { id } = useParams();
-    const [logement, setLogement] = useState({});
-
+    const [logement, setLogement] = useState(null);
+    
     useEffect(() => {
-        const find = data.find(logement => logement.id === id);
-        setLogement(find);
-    }, [id])
+        const find = data.find((logement) => logement.id === id);
+        setLogement(find || undefined);
+    }, [id]);
 
     return (
-        <main className='optimal-width'>
-            <Diapo slides={logement.pictures || []} />
-            <MainComponentRental 
-                title={logement.title} 
-                location={logement.location} 
-                tags={logement.tags} 
-                name={logement.host?.name} 
-                picture={logement.host?.picture} 
-                rating={logement.rating}
-                description={logement.description}
-                equipments={logement.equipments}
-            />
-            <InfoRental
-                description={logement.description}
-                equipments={logement.equipments}
-            />
-        </main>
-    ); 
+        // Redirection vers la page 404
+        logement === undefined
+        ? <Navigate to="*" replace />
+
+        // Recherche en attendant UseEffect
+        : logement === null
+        ? null
+        : (
+            <main className="optimal-width">
+                <Diapo slides={logement.pictures || []} />
+                <MainComponentRental
+                    title={logement.title}
+                    location={logement.location}
+                    tags={logement.tags}
+                    name={logement.host?.name}
+                    picture={logement.host?.picture}
+                    rating={logement.rating}
+                    description={logement.description}
+                    equipments={logement.equipments}
+                />
+                <InfoRental
+                    description={logement.description}
+                    equipments={logement.equipments}
+                />
+            </main>
+        )
+    );
 };
 
 export default Logement;
